324370516ec7de04e7467af824475224e48e2935
ji11ian <123039281+ji11ian@users.noreply.github.com>
2023-02-08 22:37:04 +0800

    Update HowManyPages.java

--- src/HowManyPages.java
+++ src/HowManyPages.java
@@ -2,6 +2,11 @@ public class HowManyPages {
   
   public static void main(String[] args) {
     
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter your age: ");
+    int age = input.nextInt();
+    int pages = 100 - age;
+    System.out.println(age + " year olds should read at least " + pages + " pages before giving up on a book.");
     
   }
   
